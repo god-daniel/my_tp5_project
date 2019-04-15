d348763ae9585b1dbee1c84d195ae36b28e16e87
daniel <506682730@qq.com>
2019-04-15 11:03:42 +0800

    Signed-off-by: daniel <506682730@qq.com>

--- config/app.php
+++ config/app.php
@@ -19,9 +19,9 @@ return [
     // 应用地址
     'app_host'               => '',
     // 应用调试模式
-    'app_debug'              => false,
+    'app_debug'              => true,
     // 应用Trace
-    'app_trace'              => false,
+    'app_trace'              => true,
     // 是否支持多模块
     'app_multi_module'       => true,
     // 入口自动绑定模块
@@ -140,9 +140,9 @@ return [
     // 错误显示信息,非调试模式有效
     'error_message'          => '页面错误！请稍后再试～',
     // 显示错误信息
-    'show_error_msg'         => false,
+    'show_error_msg'         => true,
     // 异常处理handle类 留空使用 \think\exception\Handle
-    'exception_handle'       => '',
+    'exception_handle'       => '\think\exception\Handle',
 	
 	'public'=>[
         'static'=>'/static'
